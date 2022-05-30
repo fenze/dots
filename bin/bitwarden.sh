019f437c9d5ec791ad848223a0baaa28e9967eae
fenze <contact@fenze.dev>
2022-05-30 10:17:21 +0200

    bw: fixed bugs

---
+++ bin/bitwarden.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Copyright (c) 2022 fenze <contact@fenze.dev>
+# See license for more information.
+
+login() {
+	[ -f /tmp/bw_session ] && [ ! -z "$(cat /tmp/bw_session)" ] && {
+		SESSION=$(cat /tmp/bw_session)
+	} || {
+		PASSWORD=$(printf "" | dmenu -P -p "Password:")
+		[ -z "$PASSWORD" ] && exit
+		printf "$PASSWORD" | xargs bw unlock --raw --nointeraction > /tmp/bw_session
+
+		[ -z "${SESSION:="$(cat /tmp/bw_session)"}" ] && {
+			$0 & exit
+		}
+	}
+
+	[ -f /tmp/bw_cache ] && [ ! -z "$(cat /tmp/bw_cache)" ] || {
+		bw list --session $SESSION --nointeraction items > /tmp/bw_cache
+	}
+
+	ITEMS=$(cat /tmp/bw_cache)
+
+	SELECT=$(echo $ITEMS | jq -r ".[] | select( has( \"login\" ) ) | \"\\(.name)\"" | dmenu)
+
+	[ -z "$SELECT" ] && exit
+
+	case $(printf 'Login\nPassword' | dmenu -p "Copy:" ) in
+		"Login")
+			echo $SELECT | xclip -sel clip;;
+		"Password")
+			bw get --session $SESSION --nointeraction password $SELECT | xclip -sel clip;;
+	esac
+}
+
+[ -z "$1" ] && login || {
+	INSTALL_PATH=/usr/bin/dmenu_bw
+	case "$1" in
+		'install') (set -x; cp $0 $INSTALL_PATH);;
+		'uninstall') (set -x; rm $INSTALL_PATH);;
+	esac
+}
